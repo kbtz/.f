76bff9d99ff8664e385fb9b78e2685aeb1f8f845
kbtz <github@kbtz.dev>
2023-08-17 19:48:23 -0300

    bump

--- cfg/rc.bash
+++ cfg/rc.bash
@@ -1,14 +1,10 @@
-export PATH=/usr/bin:~/.local/bin:~f/bin
-source .f
+export PATH=/usr/bin:/dot/.local/bin:/dot/files/bin
+source /dot/files/bin/load
 
 if [ -z $DISPLAY ] && [ `tty` = /dev/tty1 ]; then
 	vrum && big
 
-	read -p '> wake up neo...'
-	echo 'knock knock'
-	sleep 1
-
-	Hyprland > ~f/run/wm-log &
+	Hyprland > /dot/run/wm-log &
 	DISPLAY=:0 spool
 fi
 
---
+++ cfg/skel/.bashrc
@@ -0,0 +1,1 @@
+files/cfg/rc.bash
