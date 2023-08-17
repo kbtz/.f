b7ce4b4aec25bd53124d0ea84ca801e16303974f
kbtz <github@kbtz.dev>
2023-08-17 19:47:17 -0300

    wip vim

--- cfg/nvim/init.lua
+++ cfg/nvim/init.lua
@@ -2,4 +2,4 @@ cfg = vim.fn.stdpath 'config'
 package.path = cfg .. '/?;' .. package.path
 
 require 'base'
-use { vars, conf, term, bind, lang, rune, auto }
+use { vars, conf, term, misc, bind, lang, rune, auto }
